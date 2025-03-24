d8fddba0196296c701fec81afba9e36bd5da796b
Liren Tu <tuliren@gmail.com>
2025-03-24 00:37:07 -0700

    Update hero slogan

--- apps/web/src/components/Hero.tsx
+++ apps/web/src/components/Hero.tsx
@@ -38,7 +38,7 @@ export default function Hero() {
       className="flex min-h-[80vh] flex-col justify-start px-6 pb-16 pt-[20vh] text-center sm:px-8"
     >
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium leading-tight tracking-tight text-slate-700 sm:text-7xl sm:leading-tight">
-        Start <Underline text="dream project" /> with one click
+        Create a <Underline text="monorepo" /> with one click
       </h1>
       <p className="mx-auto mt-10 max-w-2xl text-xl tracking-tight text-slate-500">{DESCRIPTION}</p>
 
--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -3,7 +3,7 @@ export const MINOR_COLOR = '#6fa8dc';
 
 export const TITLE = 'Template';
 export const DESCRIPTION =
-  'This is a Turbo project template including everything you need to build your dream project.';
+  'This is a Turbo monorepo project template including everything you need to build your dream project.';
 export const DOMAIN = 'template.liren.dev';
 export const DOMAIN_URL = `https://${DOMAIN}`;
 export const CONTACT_EMAIL = `contact@${DOMAIN}`;
