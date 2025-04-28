d3ef987cb9960a6821f233fca1cdeebebf95368c
Liren Tu <tuliren@gmail.com>
2025-04-27 20:16:47 -0700

    Add one-off scripts

---
+++ apps/web/scripts/runScriptWithEnv.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check the first argument for environment selection
+ENV_CHOICE="$1"
+ENV_FILE=".env.$ENV_CHOICE.local"
+
+if [[ "$ENV_CHOICE" != "local" && "$ENV_CHOICE" != "production" && "$ENV_CHOICE" != "development" ]]; then
+  echo "Error: The first argument must be one of 'local', 'production', or 'development'."
+  exit 1
+fi
+
+shift  # Shift arguments to process any remaining options
+
+CMD="npx ts-node --project tsconfig.json"
+
+# Safety check for running in production
+if [[ $ENV_CHOICE == "production" ]]; then
+  while true; do
+    read -p "Are you sure you want to run a production script? (Y/N): " REPLY
+    case $REPLY in
+        [Yy]* ) break;;
+        [Nn]* ) exit 0;;
+        * ) echo "Please answer Y or N.";;
+    esac
+  done
+fi
+
+# Check for .env file and throw error if it does not exist
+if [[ ! -f "$ENV_FILE" ]]; then
+  echo "Error: Environment file ${ENV_FILE} does not exist."
+  exit 1
+else
+  FULL_COMMAND="DOTENV_CONFIG_PATH=$ENV_FILE $CMD -r dotenv/config -r tsconfig-paths/register $@"
+  echo "Running: $FULL_COMMAND"
+  eval $FULL_COMMAND
+fi
