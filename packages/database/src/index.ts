2d8e723886ce1253e54251eb69e0425b704448ab
Liren Tu <tuliren@gmail.com>
2025-05-14 15:28:53 -0700

    Update db module

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,9 +1,2 @@
-import { PrismaClient } from '../generated/prisma';
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+export { prisma } from './client';
+export * from '../generated/prisma';
